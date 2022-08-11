a34367c92dce4f352508f8b94f1ec4ffeda1d64e
patipan-patisampita <patipan.patisampita@gmail.com>
2022-08-11 15:33:46 +0700

    update

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'column_page.dart';
 import 'row_page.dart';
 
 class MyApp extends StatelessWidget {
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: RowPage(),
+      home: ColumnPage(),
     );
   }
 }
